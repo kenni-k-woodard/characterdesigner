622d3de0144d97f75c44ba620f0bfa604d44d5d5
Kenni Woodard <kennikristine@gmail.com>
2023-01-17 21:01:24 -0800

    added options for the rest of my selectors since theyre all working

--- src/components/main/controls/controls.js
+++ src/components/main/controls/controls.js
@@ -20,16 +20,20 @@ export default function Controls({ mask, setMask, costume, setCostume, weapon, s
       <select name="mask" value={mask} onChange={handleMaskChange}>
         <option value="jason">jason</option>
         <option value="ghostface">ghostface</option>
+        <option value="michael">michael</option>
+        <option value="leatherface">leatherface</option>
       </select>
       <label>Mask</label>
       <select name="costume" value={costume} onChange={handleCostumeChange}>
         <option value="firefighter">firefighter</option>
         <option value="flapper">flapper</option>
+        <option value="maid">maid</option>
       </select>
       <label>Costume</label>
       <select name="weapon" value={weapon} onChange={handleWeaponChange}>
         <option value="axe">axe</option>
         <option value="bat">bat</option>
+        <option value="knife">knife</option>
       </select>
       <label>Weapon</label>
     </div>
